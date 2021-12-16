12fd5de9e6b1bfbb76508036260dd38bdfa79853
rodrigosamaniego <rodrigosamaniegopiza@gmail.com>
2021-12-15 18:09:46 -0600

    estilo single guitar

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -23,6 +23,14 @@ export default function Header() {
 							</Link>
 						</div>
 					</div>
+					<div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+						<Link to="/registro" class="ml-8 whitespace-nowrap inline-flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white hover:from-purple-700 hover:to-indigo-700">
+							Crear cuenta
+						</Link>
+						<Link to="/iniciar-sesion" class="ml-8 whitespace-nowrap inline-flex items-center justify-center bg-gradient-to-r from-blue-600 to-indigo-600 bg-origin-border px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white hover:from-purple-700 hover:to-indigo-700">
+							Iniciar sesión
+						</Link>
+        			</div>
 				</div>
 			</nav>
 		</header>
